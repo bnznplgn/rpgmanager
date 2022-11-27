1184aea1ac433650e694ef9d97c03dc977b71336
Roger APLOGAN <rogercyr.aplogan@gmail.com>
2022-11-27 17:58:22 +0100

    CharacterController route rétablie

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use App\Http\Controllers\CharacterController;
-use App\Http\Controllers\GamerController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -19,7 +19,16 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::resource('/Gamers', GamerController::class);
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 
 Route::resource('/characters', CharacterController::class);
 
+require __DIR__.'/auth.php';
